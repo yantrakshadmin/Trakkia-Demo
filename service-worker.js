c8b7636d0f085ca8a8122e32eb1afef3630939d9
Rumeet Singh <singh.rumeet5@gmail.com>
2021-03-19 12:04:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.181574f9f9b6a0ad231a7dfbfc645843.js"
+  "/precache-manifest.1c4ccb1c8b6da99d2209c96441715a9c.js"
 );
 
 self.addEventListener('message', (event) => {
