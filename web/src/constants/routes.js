84f1d5b9abfe7cbf1c4026f8399cdfee39c507ab
Rumeet Singh <singh.rumeet5@gmail.com>
2021-04-21 16:01:38 +0530

    forgot password and edit profile added

--- web/src/constants/routes.js
+++ web/src/constants/routes.js
@@ -2,6 +2,11 @@ import {lazy} from 'react';
 
 export const publicRoutes = [
   {Component: lazy(() => import('../components/SignInMaster/sign-in-master.component')), path: '/'},
+  {Component: lazy(() => import('../components/forgetPassword')), path: '/forgot-password'},
+  {
+    Component: lazy(() => import('../components/confirmPassword')),
+    path: '/confirm-password/:uname',
+  },
   {
     Component: lazy(() => import('../components/SignUp/sign-up-client.component')),
     path: '/sign-up/client/',
@@ -74,7 +79,7 @@ export const outerRoutesClient = [
 export const extraRoutesEmployee = [
   {
     path: '/edit-profile/',
-    Component: lazy(() => import('screens/404.screen')),
+    Component: lazy(() => import('screens/employee/EditProfile.screen')),
   },
   {
     path: '/return-dockets/return/',
@@ -259,12 +264,12 @@ export const employeeRoutes = [
       //   path: '/main-inventory/rc-inventory-2/',
       //   Component: lazy(() => import('screens/employee/receiverClientInventory2.screen')),
       // },
-      {
-        name: 'Adjustments',
-        path: '/main-inventory/adjustments/',
-        Component: lazy(() => import('screens/employee/adjustmentInventory.screen')),
-        props: {isEmployee: true},
-      },
+      // {
+      //   name: 'Adjustments',
+      //   path: '/main-inventory/adjustments/',
+      //   Component: lazy(() => import('screens/employee/adjustmentInventory.screen')),
+      //   props: {isEmployee: true},
+      // },
     ],
   },
   {
